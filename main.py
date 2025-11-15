c5346751f21d537690ce977e7fdb13fe218e4cdf
daixinliang <daixinliang@corp.netease.com>
2025-11-15 15:14:04 +0800

    init

---
+++ main.py
@@ -0,0 +1,29 @@
+from sentence_transformers import SentenceTransformer
+import torch
+
+model = SentenceTransformer(
+    "tencent/KaLM-Embedding-Gemma3-12B-2511",
+    trust_remote_code=True,
+    model_kwargs={
+        "torch_dtype": torch.bfloat16,
+        "attn_implementation": "flash_attention_2",  # Optional
+    },
+)
+model.max_seq_length = 512
+
+sentences = ["This is an example sentence", "Each sentence is converted"]
+prompt = "Instruct: Classifying the category of french news.\nQuery:"
+embeddings = model.encode(
+    sentences,
+    prompt=prompt,
+    normalize_embeddings=True,
+    batch_size=256,
+    show_progress_bar=True,
+)
+print(embeddings)
+'''
+[[-0.01867676  0.02319336  0.00280762 ... -0.02075195  0.00196838
+  -0.0703125 ]
+ [-0.0067749   0.03491211  0.01434326 ... -0.0043335   0.00509644
+  -0.04174805]]
+'''
